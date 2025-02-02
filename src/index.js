e5a71b154e6a72c9067b3526361a426448358aa0
KAKKIRENI RAVI TEJA <ravitejak966@gmail.com>
2025-02-02 11:45:58 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import './index.css'; // <-- Add this line
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
